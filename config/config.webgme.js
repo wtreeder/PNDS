39b1c1e002ea3008cd9ffe124686d22acf31feb7
wtreeder <wtreeder@ncsu.edu>
2022-12-06 21:24:28 -0600

    Added visualizer

--- config/config.webgme.js
+++ config/config.webgme.js
@@ -11,14 +11,17 @@ config.seedProjects.basePaths.push(__dirname + '/../src/seeds/PNDS');
 
 
 
+config.visualization.panelPaths.push(__dirname + '/../src/visualizers/panels');
 
 
 
 
 // Visualizer descriptors
-
+config.visualization.visualizerDescriptors.push(__dirname + '/../src/visualizers/Visualizers.json');
 // Add requirejs paths
 config.requirejsPaths = {
+  'panels': './src/visualizers/panels',
+  'widgets': './src/visualizers/widgets',
   'wdestup': './src/common'
 };
 
